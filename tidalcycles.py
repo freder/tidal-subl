d04306f914d300f5d43e35a3ed261baa404d02ef
Frederic Brodbeck <freder.code@gmail.com>
2018-07-23 00:01:46 +0200

    expand region up and down until empty lines of end / beginning of file are reached

--- tidalcycles.py
+++ tidalcycles.py
@@ -8,11 +8,39 @@ sessionName = 'tidal'
 
 class TidalEvalCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		for region in self.view.sel():
-			line = self.view.line(region)
-			lineContents = self.view.substr(line)
-			cmd = 'tmux send-keys -t "{}" "{}" C-m'.format(
-				sessionName, 
-				lineContents.replace('"', '\\\"')
+		for selectionRegion in self.view.sel():
+			lineRegion = self.view.line(selectionRegion)
+			line = self.view.substr(lineRegion)
+			print(line)
+
+			# get surrounding lines, to evaluate entire block
+			lines = [line]
+			while True:
+				a = lineRegion.a
+				b = lineRegion.b
+				if not (a == 0 or lines[0] == ''):
+					lineRegion.a = a - 1
+				if not (b >= self.view.size() - 1 or lines[-1] == ''):
+					lineRegion.b = b + 1
+				if lineRegion.a == a and lineRegion.b == b:
+					break
+				s = self.view.substr(lineRegion)
+				lines = s.splitlines()
+				if lines[0] == '' and lines[-1] == '':
+					break
+			lines = [l for l in lines if l != '']
+			print(lines)
+
+			code = line.replace('"', '\\\"')
+			if len(lines) > 1:
+				lines = [
+					*lines
+				]
+				# TODO: evaluate multiple lines
+				# code = 
+				pass
+			os.system(
+				'tmux send-keys -t "{}" "{}" C-m \\;'.format(
+					sessionName, code
+				)
 			)
-			os.system(cmd)
