71aa3da241923c32ad4c31307969965a82596e45
Frederic Brodbeck <freder.code@gmail.com>
2018-07-22 19:52:30 +0200

    update

--- readme.md
+++ readme.md
@@ -2,12 +2,14 @@
 
 live-coding tidalcycles in sublime text. based on [vim-tidal](https://github.com/munshkr/vim-tidal).
 
+`tidal.sh` has been modified to work with `stack ghci`
+
+
 ## install
 
 - move `tidalcycles.py` to `Packages/User/`
-- get `Tidal.ghci` and `bin/tidal` from [vim-tidal](https://github.com/munshkr/vim-tidal/blob/master/Tidal.ghci)
 - adapt `tidal-subl.sh`
 
 ## key bindings
 
-`{ "keys": ["alt+enter"], "command": "tidal_eval" },`
+suggestion: `{ "keys": ["alt+enter"], "command": "tidal_eval" }`
--- tidal-subl.sh
+++ tidal-subl.sh
@@ -1,9 +1,14 @@
+#!/bin/bash
+
 SESSION="tidal"
+GHCI="stack ghci --ghci-options"
+FILE=${FILE:-"$(date +%F).tidal"}
 SAMPLES_DIR="~/Library/Application\ Support/SuperCollider/downloaded-quarks/Dirt-Samples/"
 
 tmux -2 attach-session -t $SESSION || tmux -2 \
 	new-session -s $SESSION \; \
-	send-keys -t $SESSION "open ./superdirt.sc" C-m \; \
 	send-keys -t $SESSION "open $SAMPLES_DIR" C-m \; \
-	send-keys -t $SESSION "TIDAL_BOOT_PATH=$TIDAL_BOOT_PATH TIDAL_TEMPO_IP=$TIDAL_TEMPO_IP GHCI=$GHCI tidal" C-m \; \
+	send-keys -t $SESSION "subl $FILE" C-m \; \
+	send-keys -t $SESSION "open ./superdirt.scd" C-m \; \
+	send-keys -t $SESSION "TIDAL_BOOT_PATH=$TIDAL_BOOT_PATH TIDAL_TEMPO_IP=$TIDAL_TEMPO_IP GHCI=\"$GHCI\" ./tidal.sh" C-m \; \
 	send-keys -t $SESSION ":script ./Tidal.ghci" C-m \;
