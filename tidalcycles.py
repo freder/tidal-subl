718923638aa0698f7b79c5fd132d94d599a2e654
Frederic Brodbeck <freder.code@gmail.com>
2018-07-24 11:31:34 +0200

    fixed multi-line evaluation

--- tidalcycles.py
+++ tidalcycles.py
@@ -11,16 +11,22 @@ class TidalEvalCommand(sublime_plugin.TextCommand):
 		for selectionRegion in self.view.sel():
 			lineRegion = self.view.line(selectionRegion)
 			line = self.view.substr(lineRegion)
-			print(line)
+
+			def getStrippedLine(cursorAt):
+				lineRegion = self.view.line(sublime.Region(cursorAt, cursorAt))
+				return self.view.substr(lineRegion).strip()
+
+			def startsWithWhitespace(s):
+				return s.strip()[0] != s[0]
 
 			# get surrounding lines, to evaluate entire block
 			lines = [line]
 			while True:
 				a = lineRegion.a
 				b = lineRegion.b
-				if not (a == 0 or lines[0] == ''):
+				if not (a == 0 or getStrippedLine(a - 1) == ''):
 					lineRegion.a = a - 1
-				if not (b >= self.view.size() - 1 or lines[-1] == ''):
+				if not (b >= self.view.size() - 1 or getStrippedLine(b + 1) == ''):
 					lineRegion.b = b + 1
 				if lineRegion.a == a and lineRegion.b == b:
 					break
@@ -28,13 +34,21 @@ class TidalEvalCommand(sublime_plugin.TextCommand):
 				lines = s.splitlines()
 				if lines[0] == '' and lines[-1] == '':
 					break
-			lines = [line for line in lines if line != '']
-			print(lines)
 
-			lines = [line.replace('"', '\\\"') for line in lines]
-			code = ' ; '.join(lines)
-			os.system(
-				'tmux send-keys -t "{}" "{}" C-m \\;'.format(
-					sessionName, code
+			lines = [
+				line.replace('"', '\\\"')
+				for line in lines
+				if line.strip() != ''
+			]
+			lines = [
+				line if (startsWithWhitespace(line) or i == 0) else ' ; {}'.format(line)
+				for i, line in list(enumerate(lines))
+			]
+			code = ''.join(lines)
+			codes = [c.strip() for c in code.split(';')]
+			print(codes)
+
+			for c in codes:
+				os.system(
+					'tmux send-keys -t "{}" "{}" C-m \\;'.format(sessionName, c)
 				)
-			)
